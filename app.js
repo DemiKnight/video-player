6922092ad7d70c0048ed320f68643fe4173577c0
Kryptical <alexknight133@icloud.com>
2018-10-30 17:07:41 +0000

    Basic index page + wip express routing
    1) Added basic index page, that will house the video player.
    2) Configuring the express app to send the viewer to the index page.

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+/*
+* Using template from [1] to get started with express
+*
+* Expressjs (2018) [online] <http://expressjs.com/en/starter/hello-world.html>
+* */
 const express = require('express');
 const app = express();
-const port = 3000
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+/*Serve the html files to the web*/
+app.use(express.static('www'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+/**/
+app.get('/',
+    (req, res) => res.send('Hello World!')
+);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
