2ea63819d4a3a81bcc95367a74a8d0fd3e78df0e
Alex Knight <alex.j.knight@stu.mmu.ac.uk>
2018-11-15 11:37:06 +0000

    Added buttons + WIP adding event listners
    1) Added Mute button to page
    2) WIP Added event DOMContentLoaded to also setup the video functionality with buttons
    3)

---
+++ www/js/video-player.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const videoElement = document.getElementById("main-video");
+    const videoElementControls = document.getElementById("main-video-controls");
+
+    addListeners(videoElement, videoElementControls)
+});
+
+/**
+ * Will add functionality to the buttons controlling the given video.
+ * @param videoElement Reference to the main video tag.
+ * @param videoControls Reference to the controls for the previous video.
+ */
+function addListeners(videoElement, videoControls)
+{
+
+}
