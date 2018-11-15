6ac451523620e57fcc0ba69f645a88a1036926e2
Kryptical <alexknight133@icloud.com>
2018-11-15 16:00:05 +0000

    Added New buttons WIP + WIP binding function
    1) Added Full-screen, backward, backward-step, forward, forward-step
    button. Added HTML, WIP on the Javascript.
    2) Added a binding function to addListeners, using closure, to bind the
    click event to each button. Could be expanded to include more stuff to
    bind.
    3) When playback function is undefeined, will now post an error

--- www/js/video-player.js
+++ www/js/video-player.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-
+document.addEventListener("DOMContentLoaded", () =>
+{
     const videoElement = document.getElementById("main-video");
     const videoElementControls = document.getElementById("main-video-controls");
 
@@ -8,9 +8,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function videoToggleMute(videoE)
 {
-    if(videoE === undefined) {
+    if(videoE === undefined)
+    {
         //Stop the function if the video is null/undefined, will stop errors occurring in the chat.
-        console.log(videoE);
+        console.error(videoE);
         return;
     }
     //Toggle the mute
@@ -19,9 +20,10 @@ function videoToggleMute(videoE)
 
 function videoTogglePlay(videoE) 
 {
-    if(videoE === undefined) {
+    if(videoE === undefined)
+    {
         //Stop the function if the video is null/undefined, will stop errors occurring in the chat.
-        console.log(videoE);
+        console.error(videoE);
         return;
     }
     if (videoE.paused)
@@ -40,19 +42,40 @@ function videoTogglePlay(videoE)
  */
 function addListeners(videoElement, videoControls)
 {
+    function bindClick(element, functionTOAdd, ... funcToAddParamaters)
+    {
+        console.log(funcToAddParamaters);
+        element.addEventListener("click", function () {
+            functionTOAdd(funcToAddParamaters[0]);
+        })
+    }
     // Iterate through each child tag of the visual controls, finding each button and then binding the relevant function.
     videoControls.childNodes.forEach((element) => {
         switch (element.id)
         {
             case "main-video-controls-play":
                 // The Play button.
-                element.addEventListener("click", function () { videoTogglePlay(videoElement); });
+                bindClick(element,videoTogglePlay,videoElement);
+                // element.addEventListener("click", function () { videoTogglePlay(videoElement); });
                 break;
             case "main-video-controls-mute":
                 // The mute button
                 element.addEventListener("click", function () { videoToggleMute(videoElement); });
                 break;
-            case undefined:
+            case "main  -video-controls-fullscreen":
+                console.log("Full-screen");
+                break;
+            case "main-video-controls-backward":
+                console.log("Backward");
+                break;
+            case "main-video-controls-backward-step":
+                console.log("Backward-Step");
+                break;
+            case "main-video-controls-forward":
+                console.log("Forward");
+                break;
+            case "main-video-controls-forward-step":
+                console.log("Forward-Step");
                 break;
         }
     });
