ccc85c7f51ca3f8bd762bd0d1a75137fd71d3009
Kryptical <alexknight133@icloud.com>
2018-11-15 16:15:16 +0000

    WIP Adding functionality to the new buttons
    1) Using the bindClick function, have added the functions to be trigger
    when each button is clicked. WIP As functions still need to be
    completed.

--- www/js/video-player.js
+++ www/js/video-player.js
@@ -35,6 +35,26 @@ function videoTogglePlay(videoE)
         videoE.pause();
     }
 }
+function videoToggleFullScreen(videoE) {
+    console.log("Fullscreen")
+}
+
+function videoSkipBackward(videoE)
+{
+    console.log("Backward");
+}
+function videoSkipBackwardStep(videoE)
+{
+    console.log("Backward Step");
+}
+function videoSkipForward(videoE)
+{
+    console.log("Forward");
+}
+function videoSkipForwardStep(videoE)
+{
+    console.log("Forward Step");
+}
 /**
  * Will add functionality to the buttons controlling the given video.
  * @param videoElement Reference to the main video tag.
@@ -42,9 +62,15 @@ function videoTogglePlay(videoE)
  */
 function addListeners(videoElement, videoControls)
 {
+    /**
+     *
+     * @param element Button to bind to
+     * @param functionTOAdd The function to bind the click to.
+     * @param funcToAddParamaters The parameters to pass to the aforementioned function.
+     */
     function bindClick(element, functionTOAdd, ... funcToAddParamaters)
     {
-        console.log(funcToAddParamaters);
+        // console.log(funcToAddParamaters);
         element.addEventListener("click", function () {
             functionTOAdd(funcToAddParamaters[0]);
         })
@@ -55,27 +81,33 @@ function addListeners(videoElement, videoControls)
         {
             case "main-video-controls-play":
                 // The Play button.
-                bindClick(element,videoTogglePlay,videoElement);
+                bindClick(element, videoTogglePlay,videoElement);
                 // element.addEventListener("click", function () { videoTogglePlay(videoElement); });
                 break;
             case "main-video-controls-mute":
                 // The mute button
-                element.addEventListener("click", function () { videoToggleMute(videoElement); });
+                bindClick(element, videoToggleMute, videoElement);
+                // element.addEventListener("click", function () { videoToggleMute(videoElement); });
                 break;
-            case "main  -video-controls-fullscreen":
+            case "main-video-controls-fullscreen":
                 console.log("Full-screen");
+                bindClick(element, videoToggleFullScreen, videoElement);
                 break;
             case "main-video-controls-backward":
                 console.log("Backward");
+                bindClick(element, videoSkipBackward, videoElement);
                 break;
             case "main-video-controls-backward-step":
                 console.log("Backward-Step");
+                bindClick(element, videoSkipBackwardStep, videoElement);
                 break;
             case "main-video-controls-forward":
                 console.log("Forward");
+                bindClick(element, videoSkipForward, videoElement);
                 break;
             case "main-video-controls-forward-step":
                 console.log("Forward-Step");
+                bindClick(element, videoSkipForwardStep, videoElement);
                 break;
         }
     });
