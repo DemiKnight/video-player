052c93817cf6bdae8c7fc046c70c11dafc3c2f56
Kryptical <alexknight133@icloud.com>
2018-11-22 15:19:03 +0000

    Code cleanup + docs and comments
    1) Cleaned up code and added commenst to explain choices

--- www/js/video-player.js
+++ www/js/video-player.js
@@ -4,14 +4,16 @@ document.addEventListener("DOMContentLoaded", () =>
     const videoElementControls = document.getElementById("main-video-controls");
 
     //Make sure all the video information can be accessed first.
-    videoElement.addEventListener("loadeddata", () => {
+    videoElement.addEventListener("loadeddata", () =>
+    {
         addListeners(videoElement, videoElementControls);
     });
 });
 
 function videoToggleMute(videoE)
 {
-    if (videoE !== undefined) {
+    if (videoE !== undefined)
+    {
         videoE.muted = !videoE.muted;
     } else {
         //Stop the function if the video is null/undefined, will stop errors occurring in the chat.
@@ -46,8 +48,7 @@ function videoTogglePlay(videoE)
  * -1- 'Fullscreen API' (13/11/18), Mozilla Developer Network [online] Accessed 15/11/18 <https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API> ---
  * -2- 'Fullscreen API' (02/11/18), whatwg <https://fullscreen.spec.whatwg.org/> ---
  * -3- 'Fullscreen' (18/11/14), W3C [online] Accessed 15/11/18 <https://www.w3.org/TR/fullscreen/> ---
- * @param videoE The video to apply this function to.
- * @deprecated
+ * @param {HTMLMediaElement} videoE The video to apply this function to.
  */
 function videoToggleFullScreen(videoE) {
     if(!videoE.fullscreenElement)
@@ -63,11 +64,12 @@ function videoToggleFullScreen(videoE) {
 }
 
 /**
- *
- * @param videoE The video to modify
- * @param {array} From index 1, the amount The amount of time to take/add.
+ * Add the amount given to the timestamp of the video, moving it forward or backwards.
+ * @param {HTMLMediaElement} videoE The video to modify
+ * @param {int} amount index 1, the amount The amount of time to take/add.
  */
-function videoSkip(videoE, amount) {
+function videoTimeStampAdder(videoE, amount)
+{
     console.log(amount);
     videoE.currentTime += amount; // Adds the amount passed in to the function.
 }
@@ -78,7 +80,7 @@ function videoSkip(videoE, amount) {
  *
  * -1- 'Closures' (28/05/2018), Mozilla Developer Network [online] 15/11/18 <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures>
  * @param {HTMLMediaElement} videoElement Reference to the main video tag.
- * @param videoControls Reference to the controls for the previous video.
+ * @param {HTMLDivElement} videoControls Reference to the controls for the previous video.
  */
 function addListeners(videoElement, videoControls)
 {
@@ -95,53 +97,69 @@ function addListeners(videoElement, videoControls)
     function bindClick(element, functionTOAdd, ... funcToAddParamaters)
     {
         // Bind to the 'click'
-        element.addEventListener("click", () => {
+        element.addEventListener("click", () =>
+        {
             //Any excess parameters pass into function, should always include a reference to the video.
             functionTOAdd(videoElement, ... funcToAddParamaters);
         });
-
-        // if(typeof functionOnDoubleClick === "function")
-        // element.addEventListener("dblclick", () => {
-        //     functionOnDoubleClick(videoElement, ... funcToAddParamaters)
-        // })
     }
 
     function bindDoubleClick(element, functionToCall, ... funcToAddParamaters)
     {
-        element.addEventListener("dblclick", () => {
-                functionToCall(videoElement, ... funcToAddParamaters)
-            });
+        element.addEventListener("dblclick", () =>
+        {
+            functionToCall(videoElement, ... funcToAddParamaters)
+        });
     }
 
     // Iterate through each child tag of the visual controls, finding each button and then binding the relevant function.
-    videoControls.childNodes.forEach((element) => {
+    videoControls.childNodes.forEach((element) =>
+    {
+        /*
+        videoControls is a dom object that is a static reference to the div containing all controls for the relative video.
+        As a dom object, it also contains a list with every child elements; in this case these are the controls we need to add events to.
+
+        Due to this, I don't feel the need to query the main DOM page any further, as we already have the elements. So instead,
+        iterate over every child element and attach the relevant events. Also, because we are using a reference to a specific
+        element, it can be re-usued for many videos on the same page.
+
+        I chose to use a switch statement because we will be comparing . It also naturally uses a 'strict comparison'[1]
+        for every case which is preferred Style Guide.  Though I was not aware of this when first implementing the switch statement,
+        there is a speed improvement when using switch over if...else [2], according to one source.
+
+        -1- 'switch' 9/08/2018, 'Mozilla Developer Network' [online] Accessed 22/11/2018 <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch> ---
+        -2- {switch benchmark} ~2016, 'Sam Allen'           [online] Accessed 22/11/2018 <https://www.dotnetperls.com/switch-js> ---
+         */
         switch (element.id)
         {
-            case "main-video-controls-play":
-                // The Play button.
+            case "main-video-controls-play": // The Play button.
+
                 bindClick(element, videoTogglePlay,videoElement);
-                // element.addEventListener("click", function () { videoTogglePlay(videoElement); });
+
                 break;
-            case "main-video-controls-mute":
-                // The mute button
+            case "main-video-controls-mute": // The mute button
+
                 bindClick(element, videoToggleMute, videoElement);
-                // element.addEventListener("click", function () { videoToggleMute(videoElement); });
+
                 break;
-            case "main-video-controls-fullscreen":
-                console.log("Full-screen");
+            case "main-video-controls-fullscreen": //Full screen button
+
                 bindClick(element, videoToggleFullScreen, videoElement);
                 break;
-            case "main-video-controls-backward":
-                // console.log("Backward");
-                bindClick(element, videoSkip, -10);
-                bindDoubleClick(element, videoSkip, -(videoElement.duration - videoElement.currentTime));
+            case "main-video-controls-backward": //Backwards button
+
+                bindClick(element, videoTimeStampAdder, -10); //On single click, move current time back 10 seconds
+
+                bindDoubleClick(element, videoTimeStampAdder, -(videoElement.duration - videoElement.currentTime)); //On double click, move back to beginning
+
                 break;
+            case "main-video-controls-forward": // Forward button
+
+                bindClick(element, videoTimeStampAdder, 10);
+
+                bindDoubleClick(element, videoTimeStampAdder, videoElement.currentTime + (0.25 * videoElement.duration) );
 
 
-            case "main-video-controls-forward":
-                console.log(videoElement.duration);
-                bindClick(element, videoSkip, 10);
-                bindDoubleClick(element, videoSkip, videoElement.currentTime + (0.25 * videoElement.duration) );
                 break;
         }
     });
