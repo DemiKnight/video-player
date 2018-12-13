2e0cdcc09b3af652d7952607e7b2cc8167e142f1
Kryptical <alexknight133@icloud.com>
2018-12-13 13:06:26 +0000

    WIP Condensing code to simplify
    1) Begun removing over complex code

--- www/js/video-player.js
+++ www/js/video-player.js
@@ -4,7 +4,7 @@
  * -1- J Evans, Loreto Sixth Form College, Code-Up Stockport (26/11/2018) ---
  */
 
-document.addEventListener("DOMContentLoaded", () =>
+document.addEventListener("DOMContentLoaded", loadVideo); /*() =>
 {
     const videoElement = document.getElementById("main-video");
     const videoElementControls = document.getElementById("main-video-controls");
@@ -14,42 +14,32 @@ document.addEventListener("DOMContentLoaded", () =>
     {
         addListeners(videoElement, videoElementControls);
     });
-});
+});*/
 
-/**
- * Will toggle the muted state of the video.
- * @param {HTMLMediaElement} videoElement Video to toggle the mute on.
- * @param {HTMLButtonElement} controlTag
- */
-function toggleVideoMute(videoElement, controlTag)
-{
-    //As a precaution, stop anything other than a video element from being "muted". In case another developer uses this function incorrectly.
-    if (videoElement instanceof HTMLMediaElement)
-    {
-        videoElement.muted = !videoElement.muted;
-    } else {
-        //Stop the function if the video is null/undefined, .
-        console.error("Invalid element passed to mute.");
-        console.error(videoElement);
-    }
-    if(controlTag.children[0].classList[1] === "fa-volume-up")
-    {
-        controlTag.children[0].classList.remove("fa-volume-up");
-        controlTag.children[0].classList.add("fa-volume-slash");
-    }else{
-        controlTag.children[0].classList.remove("fa-volume-slash");
-        controlTag.children[0].classList.add("fa-volume-up");
-    }
-}
 
-/**
- * Will will toggle between pause and play for the specified video.
- * @param {HTMLMediaElement} videoElement
- * @param {HTMLButtonElement} controlTag
- */
-function toggleVideoPlay(videoElement, controlTag)
+function loadVideo()
 {
-    if(videoElement instanceof HTMLMediaElement)
+    console.log("Loading video.");
+    const videoElement = document.getElementById("main-video");
+    // const videoElementControls = document.getElementById("main-video-controls");
+
+    const videoControlLocationSlider = document.getElementById("main-video-controls-locationSlider");
+    const videoControlPlayButton = document.getElementById("main-video-controls-play");
+    const videoControlMute = document.getElementById("main-video-controls-mute");
+    const videoControlFullscreen = document.getElementById("main-video-controls-fullscreen");
+    const videoControlBackward = document.getElementById("main-video-controls-backward");
+    const videoControlForward = document.getElementById("main-video-controls-forward");
+    const videoControlSpeedchanger = document.getElementById("main-video-controls-speedChanger");
+    const videoControlVolumeSlider = document.getElementById("main-video-controls-volumeSlider");
+
+    let duration = Math.round(videoElement.duration);
+
+    /**
+     * Will will toggle between pause and play for the specified video.
+     * @param {HTMLMediaElement} videoElement
+     * @param {HTMLButtonElement} controlTag
+     */
+    function toggleVideoPlay()
     {
         /*
         Alternative to the if below, using the with statement will require less calls to the original object, potentially a
@@ -75,30 +65,47 @@ function toggleVideoPlay(videoElement, controlTag)
         {
             videoElement.pause();
         }
+
+        if(videoControlPlayButton.children[0].classList[1] === "fa-play")
+        {
+            videoControlPlayButton.children[0].classList.remove("fa-play");
+            videoControlPlayButton.children[0].classList.add("fa-pause");
+        }else{
+            videoControlPlayButton.children[0].classList.remove("fa-pause");
+            videoControlPlayButton.children[0].classList.add("fa-play");
+        }
     }
 
-    if(controlTag.children[0].classList[1] === "fa-play")
+    /**
+     * Will toggle the muted state of the video.
+     * @param {HTMLMediaElement} videoElement Video to toggle the mute on.
+     * @param {HTMLButtonElement} controlTag
+     */
+    function toggleVideoMute()
     {
-        controlTag.children[0].classList.remove("fa-play");
-        controlTag.children[0].classList.add("fa-pause");
-    }else{
-        controlTag.children[0].classList.remove("fa-pause");
-        controlTag.children[0].classList.add("fa-play");
+        videoElement.muted = !videoElement.muted;
+        if(videoControlMute.children[0].classList[1] === "fa-volume-up")
+        {
+            videoControlMute.children[0].classList.remove("fa-volume-up");
+            videoControlMute.children[0].classList.add("fa-volume-slash");
+        }else{
+            videoControlMute.children[0].classList.remove("fa-volume-slash");
+            videoControlMute.children[0].classList.add("fa-volume-up");
+        }
     }
-}
 
-/**
- * Will toggle fullscreen, using the Fullscreen API [1,2]. Need to find a better way of implementing this feature due to it being discontinued [3]
- *
- * TODO Find a way to overwrite default controls when in fullscreen mode.vid
- *
- * -1- 'Fullscreen API' (13/11/18), Mozilla Developer Network [online] Accessed 15/11/18 <https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API> ---
- * -2- 'Fullscreen API' (02/11/18), whatwg <https://fullscreen.spec.whatwg.org/> ---
- * -3- 'Fullscreen' (18/11/14), W3C [online] Accessed 15/11/18 <https://www.w3.org/TR/fullscreen/> ---
- * @param {HTMLMediaElement} videoElement The video to apply this function to.
- */
-function toggleVideoFullScreen(videoElement) {
-    if(videoElement instanceof HTMLMediaElement)
+
+    /**
+     * Will toggle fullscreen, using the Fullscreen API [1,2]. Need to find a better way of implementing this feature due to it being discontinued [3]
+     *
+     * TODO Find a way to overwrite default controls when in fullscreen mode.vid
+     *
+     * -1- 'Fullscreen API' (13/11/18), Mozilla Developer Network [online] Accessed 15/11/18 <https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API> ---
+     * -2- 'Fullscreen API' (02/11/18), whatwg <https://fullscreen.spec.whatwg.org/> ---
+     * -3- 'Fullscreen' (18/11/14), W3C [online] Accessed 15/11/18 <https://www.w3.org/TR/fullscreen/> ---
+     * @param {HTMLMediaElement} videoElement The video to apply this function to.
+     */
+    function toggleVideoFullScreen()
     {
         if(!videoElement.fullscreenElement)
         {
@@ -111,7 +118,207 @@ function toggleVideoFullScreen(videoElement) {
             if(videoElement.webkitExitFullscreen() !== undefined) videoElement.webkitExitFullscreen();
         }
     }
+
+    /**
+     * Changes the volume based on the amount given.
+     * @param {HTMLMediaElement} videoE The video element to change.
+     * @param {HTMLInputElement} inputControl The value to change the volume to.
+     * @param {HTMLSpanElement} labelToUpdate Label to represent the video volume.
+     */
+    function videoVolumeChange()
+    {
+        videoElement.volume = parseFloat(videoControlVolumeSlider.children[0].value);//Turn string to float for volume usage
+
+        videoControlVolumeSlider.children[1].innerHTML = (videoElement.volume*100).toString();
+    }
+
+    function videoLocationChange()
+    {
+        videoControlLocationSlider.children[1].value = videoElement.currentTime; //Set the slider value to current video frame.
+
+        /*
+        Set the span to the current timestamp of the video. In the format hh:mm:ss.
+        The hour part is dependent of the video exceeding 3600 hours.
+        TODO Reduce the amount of code by adding seperate span tags for each numeric value or a date/time element.
+         */
+        videoControlLocationSlider.children[0].innerHTML =
+            (duration > 3600 ? Math.round(videoElement.currentTime % 3600).toString().padStart(2, '0') +  ":" : "") + //Calcuate the number of hours in video, if exceeds 1 hour
+            Math.floor(videoElement.currentTime / 60).toString().padStart(2,'0') + ":" + //Calculate minutes
+            Math.round(videoElement.currentTime % 60).toString().padStart(2,'0'); //Calculate seconds
+    }
+
+    function videoLocationChangeDrag()
+    {
+        videoElement.currentTime = videoControlLocationSlider.children[1].value;
+    }
+
+
+    function keyBindings(event)
+    {
+        switch (event.key)
+        {
+            case " ": case "Spacebar":
+
+            toggleVideoPlay();
+
+            break;
+
+            case "m":case "M":
+
+            toggleVideoMute();
+
+            break;
+
+            case "ArrowRight": case "Right":
+
+            videoTimeForwardClick();
+
+            break;
+
+            case "ArrowLeft": case "Left":
+
+            videoTimeBackwardClick();
+
+            break;
+
+            case "f": case "F":
+
+            toggleVideoFullScreen();
+
+            break;
+
+            case "ArrowUp": case "Up":
+
+            if(!((videoElement.volume + 0.1) > 1))
+            {
+
+                // videoElement.volume += parseFloat(0.1.toFixed()); //increment by 0.1
+                videoElement.volume = parseFloat((videoElement.volume + 0.1).toFixed(2));
+
+                videoControls.children[7].children[1].innerHTML = Math.round(videoElement.volume*100).toString(); //Change the label value
+
+                videoControls.children[7].children[0].value = videoElement.volume; //Change the slider value to the video control.
+            }
+
+            break;
+
+
+            case "ArrowDown": case "Down":
+
+            if(!((videoElement.volume-0.1) < 0))
+            {
+                // -= parseFloat(0.1.toFixed());
+
+                videoElement.volume = parseFloat((videoElement.volume - 0.1).toFixed(2));
+
+                videoControls.children[7].children[1].innerHTML = Math.round(videoElement.volume*100).toString(); //Change the label value
+                videoControls.children[7].children[0].value = videoElement.volume; //Change the slider value to the video control.
+
+            }
+
+            break;
+        }
+    }
+
+    function changeVisability()
+    {
+        switch (document.visibilityState)
+        {
+            case "hidden":
+                ///
+                if (!videoElement.paused) toggleVideoPlay(videoElement);
+
+
+                break;
+            case "visible":
+                //
+
+                toggleVideoPlay(videoElement);
+
+                break;
+        }
+    }
+
+    function videoTimeBackwardClick(){videoElement.currentTime -= 10;}
+    function videoTimeBackwardDoubleClick(){videoElement.currentTime = 0;}
+
+    function videoTimeForwardClick(){videoElement.currentTime += 10;}
+    function videoTimeFOrwarddDoubleClick(){videoElement.currentTime += 30;}
+
+    // ## Register Event Listeners
+
+    function defaultValues()
+    {
+        // ## Default values
+        videoVolumeChange();
+
+        videoControlLocationSlider.children[1].max = duration;
+
+        /*
+                    Set the span to the duration of the video, in the format: hh:mm:ss.
+                    The hour part is dependent of the video exceeding 3600 hours.
+
+                    Calculation for both total duration and current time.
+
+                    ## Calculations:
+
+                    - 'duration % 60' . Get the remainder after dividing by num of seconds in a minute. Then get whole number, which is the seconds.
+                    - 'duration / 60' . Get the number of minutes within the time, then remove all remaining decimals.
+
+                    - For calculating hours:
+                        - duration > 3600 . If the duration is greater than 3600, then the length is greater than a hour and should display the additional numerical values.
+                        - duration % 3600 . Get the number of hours within the duration.
+
+                     x % y : Modulus or Remainder operator [3], will get the remainding value after diving two numbers.
+
+                     Math.floor() : [1] will get the lowest integer value. Useful for getting the number of minutes within the current time regardless of the seconds to the next minute.
+                     Math.round() : [2] Will get the closest whole integer. Useful got getting the closest second after using the modulus operation.
+                     padStart(lengthToPad, fillerString) : Pad the beginning of the string with 0's, unless overridden.
+
+
+                    -1- 'Math.floor()' 29/11/2018, Mozilla Developer Network [online] Accessed 06/12/2018 <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor> ---
+                    -2- 'Math.round()' 13/07/2018, Mozilla Developer Network [online] Accessed 06/12/2018 <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round> ---
+                    -3- 'Arithmetic operators' 16/05/2018, Mozilla Developer Network [online] Accessed 06/12/2018 <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Remainder_()> ---
+                     */
+        videoControlLocationSlider.children[2].innerHTML =
+            (duration > 3600 ? Math.round(duration % 3600).toString().padStart(2, '0') +  ":" : "") + //Calcuate the number of hours in video, if exceeds 1 hour
+            Math.floor(duration / 60).toString().padStart(2,'0') + ":" + //Calculate minutes
+            Math.round(duration % 60).toString().padStart(2,'0'); //Calculate seconds
+    }
+
+    function eventListeners()
+    {
+        console.log("Event LIsteners");
+        videoControlPlayButton.addEventListener("click", toggleVideoPlay);
+
+        videoControlMute.addEventListener("click", toggleVideoMute);
+
+        videoControlFullscreen.addEventListener("click", toggleVideoFullScreen);
+
+        videoControlVolumeSlider.addEventListener("input", videoVolumeChange);
+
+        videoControlBackward.addEventListener("click", videoTimeBackwardClick);
+        videoControlBackward.addEventListener("dblclick", videoTimeBackwardDoubleClick);
+
+        videoControlForward.addEventListener("click", videoTimeForwardClick);
+        videoControlForward.addEventListener("dblclick", videoTimeFOrwarddDoubleClick);
+
+        videoControlLocationSlider.addEventListener("timeupdate", videoLocationChange);
+        videoControlLocationSlider.addEventListener("input", videoLocationChangeDrag);
+
+        defaultValues();
+    }
+
+    document.addEventListener("keydown", keyBindings);
+    document.addEventListener("visibilitychange", changeVisability);
+
+    videoElement.addEventListener("loadeddata", eventListeners);
+    
 }
+
+
+
+
 
 /**
  * Add the changeAmount given to the timestamp of the video, moving it forward or backwards.
@@ -125,18 +332,7 @@ function videoTimeStampAdder(videoElement, changeAmount)
     videoElement.currentTime += changeAmount;
 }
 
-/**
- * Changes the volume based on the amount given.
- * @param {HTMLMediaElement} videoE The video element to change.
- * @param {HTMLInputElement} inputControl The value to change the volume to.
- * @param {HTMLSpanElement} labelToUpdate Label to represent the video volume.
- */
-function videoVolumeChange(videoE, inputControl, labelToUpdate)
-{
-    videoE.volume = parseFloat(inputControl.value);//Turn string to float for volume usage
 
-    labelToUpdate.innerHTML = (videoE.volume*100).toString();
-}
 
 /**
  * Change inner HTML of a element to the current value of an input
